ff4dad5d12383957b75989696a27bb96c0973489
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2023-08-19 01:52:21 -0300

    feat: images generation page

---
+++ app/api/image/route.ts
@@ -0,0 +1,47 @@
+import { openai } from '@/lib/openai';
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+import { ChatCompletionRequestMessage } from 'openai-edge';
+
+const instructionMessage: ChatCompletionRequestMessage = {
+  role: 'system',
+  content:
+    'You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.',
+};
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { userId } = auth();
+    const { prompt, amount, resolution } = body;
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    if (!prompt) {
+      return new NextResponse('Prompt is required', { status: 400 });
+    }
+
+    if (!amount) {
+      return new NextResponse('Amount is required', { status: 400 });
+    }
+
+    if (!resolution) {
+      return new NextResponse('Resolution is required', { status: 400 });
+    }
+
+    const response = await openai.createImage({
+      prompt,
+      n: parseInt(amount, 10),
+      size: resolution,
+    });
+
+    const data = await response.json();
+
+    return NextResponse.json(data.data);
+  } catch (error) {
+    console.log('[CODE_ERROR]', error);
+    return new NextResponse('Internal server error', { status: 500 });
+  }
+}
