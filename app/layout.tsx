aaf4a73e08ecbe05e3db0b1b3103b164f7c76e01
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2023-08-21 16:56:35 -0300

    feat: crisp suport chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { CrispProvider } from '@/components/crisp-provider';
 import { ModalProvider } from '@/components/modal-provider';
 import { ToasterProvider } from '@/components/toaster-provider';
 import { ClerkProvider } from '@clerk/nextjs';
@@ -21,6 +22,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
+          <CrispProvider />
           <ToasterProvider />
           <ModalProvider />
           {children}
